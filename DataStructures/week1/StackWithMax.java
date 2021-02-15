6a963a3cf2ef0c6a521ffd1c2142150038f501de
Rijutajain <72339603+Rijutajain@users.noreply.github.com>
2021-02-15 15:02:13 +0530

    StackWithMax.java

---
+++ DataStructures/week1/StackWithMax.java
@@ -0,0 +1,73 @@
+import java.util.*;
+import java.io.*;
+
+public class StackWithMax {
+    class FastScanner {
+        StringTokenizer tok = new StringTokenizer("");
+        BufferedReader in;
+
+        FastScanner() {
+            in = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() throws IOException {
+            while (!tok.hasMoreElements())
+                tok = new StringTokenizer(in.readLine());
+            return tok.nextToken();
+        }
+        int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+    }
+
+    public void solve() throws IOException {
+        FastScanner scanner = new FastScanner();
+        int queries = scanner.nextInt();
+        Stack<Integer> stack = new Stack<Integer>();
+         Stack<Integer> auxillaryStack = new Stack<Integer>();
+    for(int q=0;q<queries;q++){
+         String operation = scanner.next();
+        if("push".equals(operation)){
+            int x=scanner.nextInt();
+            stack.push(x);
+            if(stack.size()==1){
+                auxillaryStack.push(x);
+            }
+            else{
+                if(x>auxillaryStack.peek()){
+                    auxillaryStack.push(x);
+                }
+               else{
+                      auxillaryStack.push(auxillaryStack.peek());
+                   }
+            }       
+        }
+        else if("pop".equals(operation)){
+            stack.pop();
+            auxillaryStack.pop();
+        }
+        else if("max".equals(operation)){
+            System.out.println(auxillaryStack.peek());
+        }
+        else{
+             System.out.println(" ");
+        }
+    }
+         
+        /*for (int qi = 0; qi < queries; ++qi) {
+            String operation = scanner.next();
+            if ("push".equals(operation)) {
+                int value = scanner.nextInt();
+                stack.push(value);
+            } else if ("pop".equals(operation)) {
+                stack.pop();
+            } else if ("max".equals(operation)) {
+                System.out.println(Collections.max(stack));
+            }
+        }*/
+    }
+
+    static public void main(String[] args) throws IOException {
+        new StackWithMax().solve();
+    }
+}
